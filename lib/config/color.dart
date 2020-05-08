0113023ab5899ef28aea72dfe3865ede83bb0875
Lyon Liang <lyon.liang12@gmail.com>
2020-05-08 19:34:21 +0800

    清除main.dart

---
+++ lib/config/color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class KColor {
+  // 默认主题颜色
+  static const Color primaryColor = Color.fromRGBO(132, 95, 63, 1.0);
+}
---
+++ lib/config/string.dart
@@ -0,0 +1,3 @@
+class KString {
+  static const homeTitle = 'Flutter女装商城';
+}
